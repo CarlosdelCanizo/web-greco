9164a2868042d495834eee5a053ad008987a80ff
txemasanchis <txema.sanchis@basetis.com>
2020-01-27 10:18:28 +0100

    Comentada línea prova

--- src/components/feedPanel/FeedPanel.js
+++ src/components/feedPanel/FeedPanel.js
@@ -121,8 +121,8 @@ const FeedList = ({ panelId }) => {
         )
         .then(response => {
           const newList = response.data
-          if (newList !== messagesList)
-            setMessagesList(newList);
+          // if (newList !== messagesList)
+          setMessagesList(newList);
 
         })
     }
