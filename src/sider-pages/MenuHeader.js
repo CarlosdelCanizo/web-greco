c4148d87228290e4c55028d8cbcf8afb9ae7138f
txemasanchis <txema.sanchis@basetis.com>
2020-04-24 10:46:48 +0200

    añadir paginas Sider

---
+++ src/sider-pages/MenuHeader.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Menu } from 'antd';
+import NotificationsMenu from '../header/notificationsMenu/NotificationsMenu'
+import UserMenu from '../header/userMenu/UserMenu'
+import { Badge } from 'antd'
+import './Sider.css';
+
+class MenuHeader extends React.Component {
+  state = {
+    current: 'navigation',
+  };
+
+  handleClick = e => {
+    console.log('click ', e);
+    this.setState({
+      current: e.key,
+    });
+  };
+
+  getPage() {
+    let pathname = window.location.pathname;
+    let ping = pathname.replace("ping", "")
+    let first = ping.replace("/", "");
+    let second = first.replace("-sider", "");
+    let third = second.replace(/-/g, " ");
+    let page = third.charAt(0).toUpperCase() + third.slice(1);
+    return page
+  }
+
+  render() {
+    return (
+      <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
+
+        <Menu.Item key="navigation" >
+          <div id="page-title">
+            {this.getPage()}
+          </div>
+        </Menu.Item>
+
+        <div id="userMenu">
+          <UserMenu />
+        </div>
+
+        <div id="notifications-menu">
+
+          {/* <Link to="/notifications" className="nav-text"> */}
+          <NotificationsMenu>
+            <Badge onChange={this.onChange} checked={this.state.show} dot />
+          </NotificationsMenu>
+          {/* </Link> */}
+
+        </div>
+
+      </Menu >
+    );
+  }
+}
+
+export default MenuHeader;
---
+++ src/sider-pages/PrivateMappingSider.js
@@ -0,0 +1,154 @@
+import React, { useState, useEffect } from 'react';
+import mobileLogo from '../assets/generation-solar-logo.svg'
+import { Layout, Menu, Icon, Divider, message, Popover } from 'antd';
+import { Link } from "react-router-dom";
+import PrivateMapping from '../pages/mapping/PrivateMapping';
+import './Sider.css';
+import MenuHeader from './MenuHeader';
+
+var access_token
+
+const { Header, Content, Footer, Sider } = Layout;
+
+const PrivateMappingSider = () => {
+
+  const [isLoggedIn, setLoggedIn] = useState();
+
+  useEffect(() => {
+    access_token = JSON.parse(localStorage.getItem('access_token'))
+    if (access_token === null || access_token === undefined) {
+      setLoggedIn(false);
+    } else {
+      setLoggedIn(true);
+
+    }
+  }, []);
+
+  const content = (
+    <div>
+      <p id="pop-over-content">Only for logged users!</p>
+    </div>
+  );
+
+  function addNewPanel() {
+    localStorage.setItem('currentPanelId', JSON.stringify(0))
+    localStorage.removeItem("currentPanelState")
+  }
+
+  return (
+    <Layout>
+      {/*MAIN-SIDER***************************************************************************************************/}
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => { console.log(broken); }}
+        onCollapse={(collapsed, type) => { console.log(collapsed, type); }}
+        theme="light"
+      >
+        <div className="logo" >
+          <Link to="/" >
+            <img src={mobileLogo} width="70%" height="70%" alt="background-logo" id="logo-sider" />
+          </Link>
+        </div >
+        <Divider id="sider-divider" />
+        <Menu theme="light" mode="inline" defaultSelectedKeys={['map']}>
+
+          <Menu.Item key="addNewPanel">
+            {isLoggedIn ?
+              (<Link to="/first" id={isLoggedIn ? ("logged-public-menu") : ("nav-text")} onClick={addNewPanel}>
+                <Icon type="plus-circle" /> Add Installation
+              </Link>)
+              :
+              (
+                <Popover content={content} id="popover">
+                  <div id={isLoggedIn ? ("logged-public-menu") : ("nav-text")}>
+                    <Icon type="plus-circle" /> Add installation
+                  </div>
+                </Popover>
+              )}
+          </Menu.Item>
+
+          <Menu.Item key="map">
+            {isLoggedIn ?
+              (<Link to="/private-mapping-sider" id={isLoggedIn ? ("logged-public-menu") : ("nav-text")}>
+                <Icon type="environment" /> Private Map
+              </Link>)
+              :
+              (
+                <Popover content={content} id="popover">
+                  <div id={isLoggedIn ? ("logged-public-menu") : ("nav-text")}>
+                    <Icon type="environment" /> Private Map
+                  </div>
+                </Popover>
+              )}
+          </Menu.Item>
+
+          <Menu.Item key="statistics">
+            <Link to="/statistics-sider" id="logged-public-menu">
+              <Icon type="bar-chart" /> Statistics
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="challenges">
+            {isLoggedIn ?
+              (<Link to="/my-challenges-sider" id={isLoggedIn ? ("logged-public-menu") : ("nav-text")}>
+                <Icon type="trophy" /> My Challenges
+              </Link>)
+              :
+              (
+                <Popover content={content} id="popover">
+                  <div id={isLoggedIn ? ("logged-public-menu") : ("nav-text")}>
+                    <Icon type="trophy" /> My Challenges
+                  </div>
+                </Popover>
+              )}
+          </Menu.Item>
+          <Menu.Item key="installations">
+            {isLoggedIn ?
+              (<Link to="/my-installations-sider" id={isLoggedIn ? ("logged-public-menu") : ("nav-text")}>
+                <Icon type="cluster" /> My Installations
+              </Link>)
+              :
+              (
+                <Popover content={content} id="popover">
+                  <div id={isLoggedIn ? ("logged-public-menu") : ("nav-text")}>
+                    <Icon type="cluster" /> My Installations
+                  </div>
+                </Popover>
+              )}
+          </Menu.Item>
+          <Menu.Item key="about">
+            <Link to="/about-sider" id="logged-public-menu">
+              <Icon type="exclamation-circle" /> About
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="download">
+            <Link to="/download-sider" id="logged-public-menu">
+              <Icon type="download" /> Download database
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="privacy">
+            <Link to="/privacy-policy-sider" id="logged-public-menu">
+              <Icon type="lock" /> Privacy policy
+            </Link>
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      {/*MAIN-SIDER****************************************************************************************************/}
+      <Layout>
+        <Header style={{ background: '#fff', padding: 0 }} >
+          <MenuHeader />
+        </Header>
+        <Content style={{ margin: '-16px 0 0 0' }}>
+          <div style={{ background: '#fff', minHeight: 360 }}>
+
+            <PrivateMapping />
+
+          </div>
+        </Content>
+        {/* <Footer style={{ textAlign: 'center' }}>Generation Solar ©2020 </Footer> */}
+      </Layout>
+    </Layout >
+  );
+};
+
+export default PrivateMappingSider
