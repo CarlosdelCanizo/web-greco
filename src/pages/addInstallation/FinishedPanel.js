4ef299d44a2cefa8b3fdb7fe394f6cb5d9793d77
txemasanchis <txema.sanchis@basetis.com>
2020-04-26 17:55:59 +0200

    Versió sense merge

--- src/pages/addInstallation/FinishedPanel.js
+++ src/pages/addInstallation/FinishedPanel.js
@@ -1,98 +1,3 @@
-// import React, { useState, useEffect } from "react"
-// import { Card, Button, Row, Col } from 'antd'
-// import solarPanel from '../../assets/solar-panel.svg'
-// import { Link } from "react-router-dom"
-// import axiosConfig from '../../api/axiosConfig'
-// import './finishedPanel.css'
-
-// const FinishedPanel = props => {
-
-//     const [myPanel, setMyPanel] = useState({})
-//     var access_token = 'Bearer ' + JSON.parse(localStorage.getItem('access_token'))
-//     var panel = JSON.parse(localStorage.getItem('currentPanelState'))
-//     // var multimedia = JSON.parse(localStorage.getItem('multimedia'))
-//     // console.log("MULTIMEDIA LOCAL STORAGE", multimedia)
-//     var currentPanelId = JSON.parse(localStorage.getItem("currentPanelId"))
-//     var idPanelfromUpload = JSON.parse(localStorage.getItem("idPanelfromUpload"))
-
-//     // const myPanel = props.location.myPanel;
-//     // console.log("MY PANEL DE LAS PROPS", myPanel)
-//     function getSpecificSolarPanel(id) {
-//         axiosConfig
-//             .get('/solarPanel/' + id,
-//                 {
-//                     headers: {
-//                         "Authorization": access_token
-//                     }
-//                 })
-//             .then(response => {
-//                 const data = response.data
-//                 setMyPanel({ ...data });
-//                 localStorage.setItem('myPanel', JSON.stringify(myPanel))
-//                 localStorage.removeItem("currentPanelId")
-//             })
-//     }
-
-//     if (currentPanelId > 0 && idPanelfromUpload === null) {
-//         getSpecificSolarPanel(currentPanelId);
-//     } else {
-//         getSpecificSolarPanel(idPanelfromUpload);
-//     }
-
-//     //Redirect
-//     const [toLocation, setLocation] = useState(false);
-//     function activateRedirection() {
-//         setLocation(true)
-//     }
-
-//     function addNewPanel() {
-//         localStorage.setItem('currentPanelId', JSON.stringify(0))
-//         localStorage.removeItem("currentPanelState")
-//     }
-
-//     return (
-//         <Row>
-//             {/* <div id="background-panel-register"> */}
-//             <Card id="card-panel-register-inside">
-//                 <Col span={24} xs={24} sm={24} md={24} lg={24} xl={24}>
-//                     <div>
-//                         <h2 id="tittle-consgratulations">Congratulations!</h2>
-//                         <h2 id="tittle-finished-panel"> Your installation has been </h2>
-//                         {currentPanelId === null || currentPanelId === 0 ?
-//                             <h2 id="tittle-finished-panel">added</h2> : <h2 id="tittle-finished-panel">updated</h2>}
-//                         <h2 id="tittle-finished-panel"> successfully</h2>
-//                     </div>
-//                 </Col>
-//                 <Col span={24} xs={24} sm={24} md={24} lg={24} xl={24}>
-//                     <img id="solar-panel" src={solarPanel} />
-//                 </Col>
-//                 <Col span={24} xs={24} sm={24} md={24} lg={24} xl={24}>
-//                     <div>
-//                         <Link id="add-another-installation" to="/first" onClick={addNewPanel}>
-//                             <p>+ Add another installation</p>
-//                         </Link>
-//                     </div>
-//                 </Col>
-//                 <Col span={24} xs={24} sm={24} md={24} lg={24} xl={24}>
-//                     <Link to={
-//                         {
-//                             pathname: "/show-panel-details-sider",
-//                             myPanel: { panel }
-//                         }
-//                     }>
-//                         <Button id="button-panel-finish-next" >
-//                             SEE YOUR INSTALLATION
-//                         </Button>
-//                     </Link>
-//                 </Col>
-//             </Card>
-//             {/* </div> */}
-//         </Row>
-//     )
-// }
-
-// export default FinishedPanel
-
 import React, { useState, useEffect } from "react"
 import { Card, Button, Row, Col } from 'antd'
 import solarPanel from '../../assets/solar-panel.svg'
@@ -105,62 +10,34 @@ const FinishedPanel = props => {
     const [myPanel, setMyPanel] = useState({})
     var access_token = 'Bearer ' + JSON.parse(localStorage.getItem('access_token'))
     var panel = JSON.parse(localStorage.getItem('currentPanelState'))
-    // var multimediaInitialState = JSON.parse(localStorage.getItem('multimedia'))
+    // var multimedia = JSON.parse(localStorage.getItem('multimedia'))
+    // console.log("MULTIMEDIA LOCAL STORAGE", multimedia)
     var currentPanelId = JSON.parse(localStorage.getItem("currentPanelId"))
-    var idPanelFromPostPanel = JSON.parse(localStorage.getItem("idPanelFromPostPanel"))
-
-
-    //GET SPECIFIC PANEL
-
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const result = await axiosConfig(
-                '/solarPanel/' + idPanelFromPostPanel,
-                {
-                    headers: {
-                        "Content-Type": "application/json",
-                        "Authorization": access_token
-                    }
-                }
-            );
-            var data = result.data
-            setMyPanel(data);
-        };
-        fetchData();
-
-        console.log("data from panel:", myPanel)
-    }, []);
-
-    // useEffect(() => {
-    // if (currentPanelId > 0 && idPanelFromPostPanel === null) {
-    //  getSpecificSolarPanel(idPanelFromPostPanel);
-    // } else {
-    //     getSpecificSolarPanel(idPanelFromPostPanel);
-    // }
-    // }, []);
-
+    var idPanelfromUpload = JSON.parse(localStorage.getItem("idPanelfromUpload"))
 
     // const myPanel = props.location.myPanel;
     // console.log("MY PANEL DE LAS PROPS", myPanel)
+    function getSpecificSolarPanel(id) {
+        axiosConfig
+            .get('/solarPanel/' + id,
+                {
+                    headers: {
+                        "Authorization": access_token
+                    }
+                })
+            .then(response => {
+                const data = response.data
+                setMyPanel({ ...data });
+                localStorage.setItem('myPanel', JSON.stringify(myPanel))
+                localStorage.removeItem("currentPanelId")
+            })
+    }
 
-    // function getSpecificSolarPanel(id) {
-    //     axiosConfig
-    //         .get('/solarPanel/' + id,
-    //             {
-    //                 headers: {
-    //                     "Authorization": access_token
-    //                 }
-    //             })
-    //         .then(response => {
-    //             const data = response.data
-    //             console.log("EL RESPONSE DATA", data)
-    //             // console.log("EL RESPONSE ", response)
-    //             setMyPanel({ ...data });
-    //             localStorage.setItem('myPanel', JSON.stringify(data))
-    //             // localStorage.removeItem("currentPanelId")
-    //         })
-    // }
+    if (currentPanelId > 0 && idPanelfromUpload === null) {
+        getSpecificSolarPanel(currentPanelId);
+    } else {
+        getSpecificSolarPanel(idPanelfromUpload);
+    }
 
     //Redirect
     const [toLocation, setLocation] = useState(false);
@@ -203,7 +80,7 @@ const FinishedPanel = props => {
                             myPanel: { panel }
                         }
                     }>
-                        <Button id="button-panel-finish-next" onClick={localStorage.removeItem("idPanelFromPostPanel")}>
+                        <Button id="button-panel-finish-next" >
                             SEE YOUR INSTALLATION
                         </Button>
                     </Link>
--- src/utils/profile/ProfileContext.js
+++ src/utils/profile/ProfileContext.js
@@ -1,67 +1,8 @@
-// import React, { createContext, useState, useEffect } from 'react';
-// import axiosConfig from '../../api/axiosConfig';
-// export const ProfileContext = createContext();
-
-// const ProfileProvider = ({ children, props }) => {
-
-//   const userProfile = {
-//     username: '',
-//     email: '',
-//     isPreviouslyLogged: false,
-//     isLoggedin: false
-//   }
-
-//   const [userInfo, setUserInfo] = useState({ ...userProfile });
-//   const [isLoggedin, setLoggedIn] = useState(false);
-
-//   useEffect(() => {
-//     var access_token = 'Bearer ' + JSON.parse(localStorage.getItem('access_token'));
-//     if (access_token) {
-//       axiosConfig.get("/users/getMyUserInfo",
-//         {
-//           headers: {
-//             "Content-Type": 'application/json',
-//             "Authorization": access_token
-//           }
-//         })
-//         .then(result => {
-//           setUserInfo({ ...result.data })
-//           setLoggedIn(true)
-//           console.log("UserInfo Efrén: ", result.data)
-//           //window.location.href = '/private-mapping-sider';
-//           //props.history.push('/private-mapping-sider')
-//         });
-//     }
-//   }, []);
-
-//   const username = userInfo.username;
-//   const email = userInfo.email;
-//   const isPreviouslyLogged = userInfo.isPreviouslyLogged;
-
-//   return (
-//     <ProfileContext.Provider
-//       value={{
-//         username,
-//         email,
-//         isLoggedin,
-//         isPreviouslyLogged,
-//         setUserInfo,
-//         setLoggedIn
-//       }}
-//     >
-//       {children}
-//     </ProfileContext.Provider>
-//   );
-// };
-
-// export default ProfileProvider;
-
 import React, { createContext, useState, useEffect } from 'react';
 import axiosConfig from '../../api/axiosConfig';
-
 export const ProfileContext = createContext();
 
-const ProfileProvider = ({ children }) => {
+const ProfileProvider = ({ children, props }) => {
 
   const userProfile = {
     username: '',
@@ -86,6 +27,9 @@ const ProfileProvider = ({ children }) => {
         .then(result => {
           setUserInfo({ ...result.data })
           setLoggedIn(true)
+          console.log("UserInfo Efrén: ", result.data)
+          //window.location.href = '/private-mapping-sider';
+          //props.history.push('/private-mapping-sider')
         });
     }
   }, []);
@@ -111,3 +55,59 @@ const ProfileProvider = ({ children }) => {
 };
 
 export default ProfileProvider;
+
+// import React, { createContext, useState, useEffect } from 'react';
+// import axiosConfig from '../../api/axiosConfig';
+
+// export const ProfileContext = createContext();
+
+// const ProfileProvider = ({ children }) => {
+
+//   const userProfile = {
+//     username: '',
+//     email: '',
+//     isPreviouslyLogged: false,
+//     isLoggedin: false
+//   }
+
+//   const [userInfo, setUserInfo] = useState({ ...userProfile });
+//   const [isLoggedin, setLoggedIn] = useState(false);
+
+//   useEffect(() => {
+//     var access_token = 'Bearer ' + JSON.parse(localStorage.getItem('access_token'));
+//     if (access_token) {
+//       axiosConfig.get("/users/getMyUserInfo",
+//         {
+//           headers: {
+//             "Content-Type": 'application/json',
+//             "Authorization": access_token
+//           }
+//         })
+//         .then(result => {
+//           setUserInfo({ ...result.data })
+//           setLoggedIn(true)
+//         });
+//     }
+//   }, []);
+
+//   const username = userInfo.username;
+//   const email = userInfo.email;
+//   const isPreviouslyLogged = userInfo.isPreviouslyLogged;
+
+//   return (
+//     <ProfileContext.Provider
+//       value={{
+//         username,
+//         email,
+//         isLoggedin,
+//         isPreviouslyLogged,
+//         setUserInfo,
+//         setLoggedIn
+//       }}
+//     >
+//       {children}
+//     </ProfileContext.Provider>
+//   );
+// };
+
+// export default ProfileProvider;
