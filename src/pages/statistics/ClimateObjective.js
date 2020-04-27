cc289cbb016157f835accf950527446e1c477c08
txemasanchis <txema.sanchis@basetis.com>
2020-04-27 17:22:00 +0200

    Añadido climate Objective

--- src/pages/statistics/ClimateObjective.js
+++ src/pages/statistics/ClimateObjective.js
@@ -1,79 +1,16 @@
-// import React from 'react'
-// import { Tag, Slider } from 'antd';
-// import './Statistics.css';
-// import axiosConfig from '../../api/axiosConfig';
-
-// const marks = {
-//   0: '0GW',
-//   17: '17GW',
-//   27: '27GW',
-//   37: {
-//     style: {
-//       color: '#db4196',
-//     },
-//     label: <strong>37GW</strong>,
-//   },
-// };
-
-// class ClimateObjective extends React.Component {
-
-//   // componentDidMount() {
-//   //   axiosConfig
-//   //     .get('/co2saving')
-//   //     .then(response => {
-//   //       var response = response.data;
-//   //       console.log(response)
-//   //     });
-//   // }
-
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <div id="slider">
-//           {/* <Tooltip title="3 done / 3 in progress / 4 to do"> */}
-
-//           <div>
-//             <Slider range marks={marks} min={0} max={37} defaultValue={[3, 27]} id="slider-climate-objective" disabled />
-//           </div>
-
-//           {/* <Progress
-//             percent={100}
-//             successPercent={56}
-//             format={() => 'GW'}
-//             id="climate-objective-progress" /> */}
-
-//         </div>
-//         {/* <div id="measurements-left">
-//           <Tag color="#2a4092" id="tag">13</Tag>
-//           <p>Total PV today</p>
-//         </div>
-//         <div id="measurements-right">
-//           <Tag color="#db4196" id="tag">3</Tag>
-//           <p>Citizen owned PV</p>
-//         </div> */}
-//       </React.Fragment>
-//     );
-//   }
-// }
-
-// export default ClimateObjective
-
-
 import React from 'react'
-import { Tag, Slider } from 'antd';
+import { Tag, Slider, Col, Row } from 'antd';
 import './Statistics.css';
 import axiosConfig from '../../api/axiosConfig';
 
 const marks = {
-  0: '0GW',
-  25: '25GW',
-  50: '50GW',
-  75: '75GW',
-  100: {
+  0: '0 Tm',
+  400000: '400.000 Tm',
+  800000: {
     style: {
       color: '#db4196'
     },
-    label: <strong>100GW</strong>
+    label: <strong>800.000 Tm</strong>
   }
 };
 
@@ -83,21 +20,14 @@ class ClimateObjective extends React.Component {
     this.state = {
       minCo2: 0,
       maxCo2: 0
-    }
+    };
   }
-
 
   getCo2Saving() {
     axiosConfig
       .get('/co2saving')
       .then(response => {
-        const minCo2 = response.data.minCo2
-        const maxCo2 = response.data.maxCo2
-        this.setState({ minCo2: response.data.minCo2 });
-        this.setState({ maxCo2: response.data.maxCo2 });
-        console.log("PROVA STATE MIN", this.state.minCo2)
-        console.log("PROVA STATE MAX", this.state.maxCo2)
-
+        this.setState({ minCo2: response.data.minCo2, maxCo2: response.data.maxCo2 });
       });
   }
 
@@ -108,20 +38,32 @@ class ClimateObjective extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <div id="slider">
-          <div>
-            <Slider range marks={marks} min={0} max={100} defaultValue={[30, 50]} id="slider-climate-objective" disabled />
-          </div>
-          <p id="small-letters-installed">This value is an estimate based on global parameters</p>
-        </div>
-        <div id="measurements-left">
-          <Tag color="#2a4092" id="tag">13</Tag>
-          <p>Total PV today</p>
-        </div>
-        <div id="measurements-right">
-          <Tag color="#db4196" id="tag">3</Tag>
-          <p>Citizen owned PV</p>
-        </div>
+        <Col>
+          <Row >
+            <div id="div-slider-climate">
+              <Slider range marks={marks} min={0} max={800000}
+                value={[this.state.minCo2, this.state.maxCo2]}
+                disabled />
+            </div>
+          </Row>
+          <Row>
+            <p id="small-letters-climate">This value is an estimate based on global parameters</p>
+          </Row>
+          <Row>
+            {/* <Col xs={12} sm={12} md={12} lg={12} xl={12} >
+              <div id="measurements-left">
+                <Tag color="#2a4092" id="tag">13</Tag>
+                <p>Total PV today</p>
+              </div>
+            </Col>
+            <Col xs={12} sm={12} md={12} lg={12} xl={12} >
+              <div id="measurements-right">
+                <Tag color="#db4196" id="tag">3</Tag>
+                <p>Citizen owned PV</p>
+              </div>
+            </Col> */}
+          </Row>
+        </Col>
       </React.Fragment>
     );
   }
--- src/pages/statistics/Statistics.js
+++ src/pages/statistics/Statistics.js
@@ -45,7 +45,6 @@ class Statistics extends React.Component {
       <React.Fragment >
         <Row id="statistics-row">
 
-
           <Col xs={24} sm={12} md={12} lg={12} xl={12} >
             <div id="statistics-map-container" >
               <PanelsHeatMap />
@@ -54,7 +53,6 @@ class Statistics extends React.Component {
               <p id="heatmap-explanation">Heat map showing the geographical distribution and amount of installed capacity of photovoltaic installations registered in Generation Solar.</p>
             </div>
           </Col>
-
 
           <Col id="container-cards" xs={24} sm={12} md={12} lg={12} xl={12} >
             <Row >
@@ -89,12 +87,13 @@ class Statistics extends React.Component {
 
           <Col id="container-cards" span={24} xs={24} sm={12} md={12} lg={12} xl={12} >
             <div id="climate-objective-card-statistics-container" >
-              <h2 id="tittle-installed">CLIMATE OBJECTIVE</h2>
+              <h2 id="tittle-installed">CLIMATE OBJECTIVE FOR 2030</h2>
               <div>
                 <Row>
-                  <h1 style={{ color: "#2a4092" }} id="numbers-climate-objective" >37</h1>
-                  <p id="letters-climate-objective">GW</p>
-                  <p id="small-letters-climate">Target 2030</p>
+                  <h1 style={{ color: "#2a4092" }} id="numbers-climate-objective" >800.000</h1>
+                </Row>
+                <Row>
+                  <p id="letters-climate-objective">Tonnes of CO₂</p>
                 </Row>
               </div>
               <ClimateObjective />
@@ -107,119 +106,3 @@ class Statistics extends React.Component {
 }
 
 export default Statistics
-
-
-// import React from 'react';
-// import './Statistics.css';
-// import { Row, Col, Divider } from 'antd';
-// import PanelsHeatMap from './PanelsHeatMap';
-// import ClimateObjective from './ClimateObjective';
-// import axiosConfig from '../../api/axiosConfig';
-
-// var position = { lat: 39.8714243295929, lng: -0.06466403603553773 }
-
-// const yerarObjective = 400 //tonnes CO2
-
-// class Statistics extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       totalInstallations: '',
-//       totalPower: ''
-//     };
-//   }
-
-//   getAllInstallationsNumber() {
-//     axiosConfig({
-//       url: '/solarPanel/gettotalnumber',
-//       method: 'GET'
-//     }).then(response => {
-//       this.setState({ totalInstallations: response.data });
-//     });
-//   }
-
-//   getAllInstallationsPower() {
-//     axiosConfig({
-//       url: '/solarPanel/gettotalenergy',
-//       method: 'GET'
-//     }).then(response => {
-//       this.setState({ totalPower: response.data });
-//     });
-//   }
-
-
-//   componentDidMount() {
-//     this.getAllInstallationsNumber();
-//     this.getAllInstallationsPower();
-//   }
-
-//   render() {
-//     document.body.classList.remove('body_forms');
-//     return (
-//       <React.Fragment >
-//         <Row id="statistics-row">
-
-
-//           <Col xs={24} sm={12} md={12} lg={12} xl={12} >
-//             <div id="statistics-map-container" >
-//               <PanelsHeatMap />
-//             </div>
-//             <div>
-//               <p id="heatmap-explanation">Heat map showing the geographical distribution and amount of installed capacity of photovoltaic installations registered in Generation Solar.</p>
-//             </div>
-//           </Col>
-
-
-//           <Col id="container-cards" xs={24} sm={12} md={12} lg={12} xl={12} >
-//             <Row >
-//               <div id="capacity-card-statistics-container" >
-//                 <h2 id="tittle-installed">NUMBER OF INSTALLATIONS</h2>
-//                 <h1 style={{ color: "#2a4092" }} id="numbers-installed">
-//                   {this.state.totalInstallations}
-//                 </h1>
-//                 <p style={{ color: "#bfc5d2" }} id="small-letters-installed">
-//                   Photovoltaic installations registered in Generation Solar
-//                 </p>
-//                 <Divider />
-//                 <h2 id="tittle-installed">TOTAL CAPACITY</h2>
-//                 <div>
-//                   <Col span={18}>
-//                     <h1 style={{ color: "#2a4092" }} id="numbers-total-capacity" >
-//                       {this.state.totalPower}
-//                     </h1>
-//                   </Col>
-//                   <Col span={6}>
-//                     <p id="big-letters-installed">kW</p>
-//                   </Col>
-//                   <Row>
-//                     <p style={{ color: "#bfc5d2" }} id="small-letters-installed">
-//                       Sum of the installed capacity shared in Generation Solar.
-//                   </p>
-//                   </Row>
-//                 </div>
-//               </div>
-//             </Row>
-//           </Col>
-
-//           {/* 
-//           <Col id="container-cards" span={24} xs={24} sm={12} md={12} lg={12} xl={12} >
-//             <div id="climate-objective-card-statistics-container" >
-//               <h2 id="tittle-installed">CLIMATE OBJECTIVE</h2>
-//               <div>
-//                 <Row>
-//                   <h1 style={{ color: "#2a4092" }} id="numbers-climate-objective" >37</h1>
-//                   <p id="letters-climate-objective">GW</p>
-//                   <p id="small-letters-climate">Target 2030</p>
-//                 </Row>
-//               </div>
-//               <ClimateObjective />
-//             </div>
-//           </Col>
-// } */}
-//         </Row>
-//       </React.Fragment >
-//     );
-//   }
-// }
-
-// export default Statistics
