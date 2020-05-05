f3b9d516228b97b72fc4e6c47fb84f7ca21058c6
txemasanchis <txema.sanchis@basetis.com>
2020-05-05 02:31:29 +0200

    Més canvis my intallation

--- src/pages/myInstallations/MyInstallations.js
+++ src/pages/myInstallations/MyInstallations.js
@@ -10,10 +10,10 @@ function MyInstallations() {
     <React.Fragment>
       <div id="background-installations">
         <Row id="background-row" >
-          <Col span={24} xs={24} sm={24} md={6} lg={4} xl={4} xxl={3}>
+          <Col span={24} xs={24} sm={24} md={6} lg={5} xl={4} xxl={3}>
             <ButtonAdd />
           </Col>
-          <Col span={24} xs={24} sm={24} md={18} lg={20} xl={20} xxl={21}>
+          <Col span={24} xs={24} sm={24} md={18} lg={19} xl={20} xxl={21}>
             <PanelList />
           </Col>
         </Row>
